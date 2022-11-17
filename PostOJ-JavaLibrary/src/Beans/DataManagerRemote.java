3d6a8687af36784f4edf3d4f9c2b2345b61dcd9f
JimPyh <100441614+JimPyh@users.noreply.github.com>
2022-11-17 09:47:16 +0100

    Add files via upload

---
+++ PostOJ-JavaLibrary/src/Beans/DataManagerRemote.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Beans;
+
+import javax.ejb.Remote;
+
+/**
+ *
+ * @author r0723037
+ */
+@Remote
+public interface DataManagerRemote {
+    
+}
